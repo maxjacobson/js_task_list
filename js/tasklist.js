1e83dfc199ac9c18f658630393a69da0ca1575f9
Max Jacobson <maxwell.jacobson@gmail.com>
2013-06-29 21:31:33 -0400

    tabs to spaces! no ds store! ahh!

--- js/tasklist.js
+++ js/tasklist.js
@@ -1,30 +1,30 @@
 $(document).ready(function(){
 
-	var new_task, remove_task_num;
+  var new_task, remove_task_num;
 
-	$("#add_task").on("submit", function(event) {
+  $("#add_task").on("submit", function(event) {
 
-		// tells the browser to stay put
-		event.preventDefault();
+    // tells the browser to stay put
+    event.preventDefault();
 
-		// get the new task and reset the input to be blank
-		new_task = $("#task_name").val();
-		$("#task_name").val("");
+    // get the new task and reset the input to be blank
+    new_task = $("#task_name").val();
+    $("#task_name").val("");
 
-		// append the new task to the list
-		$("<li>" + new_task + "</li>").appendTo("#task_list");
+    // append the new task to the list
+    $("<li>" + new_task + "</li>").appendTo("#task_list");
 
-	});
+  });
 
-	$("#remove_task").on("submit", function(event) {
-		event.preventDefault();
+  $("#remove_task").on("submit", function(event) {
+    event.preventDefault();
 
-		// gets the number. assumes the user doesn't know about zero indexed lists
-		remove_task_num = parseInt($("#task_num").val()) - 1;
-		$("#task_num").val("");
+    // gets the number. assumes the user doesn't know about zero indexed lists
+    remove_task_num = parseInt($("#task_num").val()) - 1;
+    $("#task_num").val("");
 
-		// removes the item
-		$("#task_list li").eq(remove_task_num).remove();
-	});
-	
+    // removes the item
+    $("#task_list li").eq(remove_task_num).remove();
+  });
+  
 });
